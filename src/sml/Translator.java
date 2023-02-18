504fba017133dbcead5e66c1f87a890581527997
Lewis Hickley <lahickley@gmail.com>
2023-02-18 16:52:07 +0000

    Added `DivInstruction` handling to `Translator.java`

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -82,6 +82,11 @@ public final class Translator {
                 String s = scan();
                 return new MulInstruction(label, Register.valueOf(r), Register.valueOf(s));
             }
+            case DivInstruction.OP_CODE -> {
+                String r = scan();
+                String s = scan();
+                return new DivInstruction(label, Register.valueOf(r), Register.valueOf(s));
+            }
 
             // TODO: add code for all other types of instructions
 
