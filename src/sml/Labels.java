a2a44af5f97b69e099e57d2d3cfa107acb2c5844
Lewis Hickley <lahickley@gmail.com>
2023-02-19 02:42:42 +0000

    Implemented toString for `Labels` and removed the relevant TODO

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -3,6 +3,7 @@ package sml;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 // TODO: write a JavaDoc for the class
 
@@ -55,8 +56,10 @@ public final class Labels {
 	 */
 	@Override
 	public String toString() {
-		// TODO: Implement the method using the Stream API (see also class Registers).
-		return "";
+		return labels.entrySet()
+				.stream()
+				.map(e -> e.getKey() + " -> " + e.getValue())
+				.collect(Collectors.joining(","));
 	}
 
 	/**
