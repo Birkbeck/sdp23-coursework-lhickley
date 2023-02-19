bc84176e6f7fabcb54c2029bb6af0e0bb154d45b
Lewis Hickley <lahickley@gmail.com>
2023-02-19 17:50:47 +0000

    Removed an unused import

--- test/sml/TranslatorTest.java
+++ test/sml/TranslatorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.ArrayList;
 
 public class TranslatorTest {
     private Machine machine;
