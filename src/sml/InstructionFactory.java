92dccfb67bb39a4febadb3597da3fe2bd626655a
Lewis Hickley <lahickley@gmail.com>
2023-02-25 17:14:54 +0000

    Finished adding creation methods to the Singleton InstructionFactory class

--- src/sml/InstructionFactory.java
+++ src/sml/InstructionFactory.java
@@ -3,6 +3,8 @@ package sml;
 import sml.instruction.*;
 import static sml.Registers.Register;
 
+//TODO Add some error handling to all of this
+
 public class InstructionFactory {
 
     private static InstructionFactory instance = null;
@@ -22,7 +24,16 @@ public class InstructionFactory {
                 return new AddInstruction(label, Register.valueOf(args[0]), Register.valueOf(args[1]));
             case "sub":
                 return new SubInstruction(label, Register.valueOf(args[0]), Register.valueOf(args[1]));
-            // TODO Add the other instructions
+            case "mul":
+                return new MulInstruction(label, Register.valueOf(args[0]), Register.valueOf(args[1]));
+            case "div":
+                return new DivInstruction(label, Register.valueOf(args[0]), Register.valueOf(args[1]));
+            case "out":
+                return new OutInstruction(label, Register.valueOf(args[0]));
+            case "mov":
+                return new MovInstruction(label, Register.valueOf(args[0]), Integer.parseInt(args[1]));
+            case "jnz":
+                return new JnzInstruction(label, Register.valueOf(args[0]), args[1]);
             default:
                 throw new IllegalArgumentException("Unknown instruction: " + opcode);
         }
