f5030d4a71a47597baf6cc5afe627f47394d8970
Lewis Hickley <lahickley@gmail.com>
2023-02-26 02:38:27 +0000

    Error handling added to `InstructionFactory`

--- src/sml/InstructionFactory.java
+++ src/sml/InstructionFactory.java
@@ -3,6 +3,7 @@ package sml;
 import sml.instruction.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static sml.Registers.Register;
 
@@ -12,7 +13,8 @@ public class InstructionFactory {
 
     private static InstructionFactory instance = null;
 
-    private InstructionFactory() {}
+    private InstructionFactory() {
+    }
 
     public static InstructionFactory getInstance() {
         if (instance == null) {
@@ -22,24 +24,40 @@ public class InstructionFactory {
     }
 
     public Instruction create(String label, String opcode, ArrayList<String> args) {
-        switch (opcode) {
-            case "add":
-                return new AddInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
-            case "sub":
-                return new SubInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
-            case "mul":
-                return new MulInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
-            case "div":
-                return new DivInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
-            case "out":
-                return new OutInstruction(label, Register.valueOf(args.get(0)));
-            case "mov":
-                return new MovInstruction(label, Register.valueOf(args.get(0)), Integer.parseInt(args.get(1)));
-            case "jnz":
-                return new JnzInstruction(label, Register.valueOf(args.get(0)), args.get(1));
-            default:
-                throw new IllegalArgumentException("Unknown instruction: " + opcode);
+        try {
+            if (opcode.equals("out")) {
+                if (args.size() != 1) {
+                    throw new RuntimeException("For out instructions, only one argument is accepted after the opcode");
+                }
+            } else {
+                if (args.size() != 2) {
+                    throw new RuntimeException("for " + opcode + " instructions, only two arguments are accepted after the opcode");
+                }
+            }
+            switch (opcode) {
+                case "add":
+                    return new AddInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
+                case "sub":
+                    return new SubInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
+                case "mul":
+                    return new MulInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
+                case "div":
+                    return new DivInstruction(label, Register.valueOf(args.get(0)), Register.valueOf(args.get(1)));
+                case "out":
+                    return new OutInstruction(label, Register.valueOf(args.get(0)));
+                case "mov":
+                    return new MovInstruction(label, Register.valueOf(args.get(0)), Integer.parseInt(args.get(1)));
+                case "jnz":
+                    return new JnzInstruction(label, Register.valueOf(args.get(0)), args.get(1));
+                default:
+                    throw new IllegalArgumentException("Unknown instruction: " + opcode);
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal arguments passed for " + opcode);
+            System.out.println("Permitted register values are: ");
+            System.out.println(Arrays.toString(Register.values()));
         }
+        return null;
     }
-
 }
+
