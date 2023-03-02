f1ede92969d2f9411379c527723b41d56f66ec98
Lewis Hickley <lahickley@gmail.com>
2023-03-02 16:45:08 +0000

    Added handling around int overflows for `AddInstruction` and tests associated with this.

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -26,6 +26,13 @@ public class AddInstruction extends Instruction {
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
 		int value2 = m.getRegisters().get(source);
+		int res = value1 + value2;
+		if (((value1 ^ res) & (value2 ^ res)) < 0) {
+			throw new ArithmeticException("The addition of values " + value1 + " and " + value2 + " stored in the " +
+					"registers " + result + " and " + source + " cannot be summed.\nThis will lead to a value " +
+					"overflow in the " + result + " register.\nThe maximum value which can be summed to is 2,147,483,647"
+			);
+		}
 		m.getRegisters().set(result, value1 + value2);
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
