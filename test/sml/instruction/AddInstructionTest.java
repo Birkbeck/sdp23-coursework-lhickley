2331a4ec6947039ad11514b43ea7cc3edb6966e5
Lewis Hickley <lahickley@gmail.com>
2023-03-03 03:56:35 +0000

    Autoformatted `AddInstructionTest` to bring it in line with the other classes in the package.

--- test/sml/instruction/AddInstructionTest.java
+++ test/sml/instruction/AddInstructionTest.java
@@ -13,93 +13,93 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static sml.Registers.Register.*;
 
 class AddInstructionTest {
-  private Machine machine;
-  private Registers registers;
+    private Machine machine;
+    private Registers registers;
 
-  @BeforeEach
-  void setUp() {
-    machine = new Machine(new Registers());
-    registers = machine.getRegisters();
-  }
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+    }
 
-  @AfterEach
-  void tearDown() {
-    machine = null;
-    registers = null;
-  }
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
 
-  @Test
-  void executeValid() {
-    registers.set(EAX, 5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(11, machine.getRegisters().get(EAX));
-  }
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(11, machine.getRegisters().get(EAX));
+    }
 
-  @Test
-  void executeValidTwo() {
-    registers.set(EAX, -5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(1, machine.getRegisters().get(EAX));
-  }
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -5);
+        registers.set(EBX, 6);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+    }
 
-  @Test
-  void executeToStringNoLabel() {
-    registers.set(EAX, 6);
-    registers.set(EBX, 5);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    String toStringResult = instruction.toString();
-    Assertions.assertEquals("add EAX EBX", toStringResult);
-  }
+    @Test
+    void executeToStringNoLabel() {
+        registers.set(EAX, 6);
+        registers.set(EBX, 5);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        String toStringResult = instruction.toString();
+        Assertions.assertEquals("add EAX EBX", toStringResult);
+    }
 
-  @Test
-  void executeToStringWithLabel() {
-    registers.set(EAX, 6);
-    registers.set(EBX, 5);
-    Instruction instruction = new AddInstruction("test", EAX, EBX);
-    String toStringResult = instruction.toString();
-    Assertions.assertEquals("test: add EAX EBX", toStringResult);
-  }
+    @Test
+    void executeToStringWithLabel() {
+        registers.set(EAX, 6);
+        registers.set(EBX, 5);
+        Instruction instruction = new AddInstruction("test", EAX, EBX);
+        String toStringResult = instruction.toString();
+        Assertions.assertEquals("test: add EAX EBX", toStringResult);
+    }
 
-  //Java will treat an unset int as a 0, as int is a primitive and so can't be set to null
-  //Here we confirm that this ends up being treated as such when doing an addition
-  @Test
-  void attemptToUseNullRegister() {
-    registers.set(EAX, 1);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(1, registers.get(EAX));
-  }
+    //Java will treat an unset int as a 0, as int is a primitive and so can't be set to null
+    //Here we confirm that this ends up being treated as such when doing an addition
+    @Test
+    void attemptToUseNullRegister() {
+        registers.set(EAX, 1);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(1, registers.get(EAX));
+    }
 
-  @Test
-  void executeMaxIntegerOverflow() {
-    registers.set(EAX, 2147483647);
-    registers.set(EBX, 1);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    Exception exception = assertThrows(ArithmeticException.class, () -> instruction.execute(machine));
-    String expectedMessage = """
-            The combination of values 2147483647 and 1 stored in the registers EAX and EBX using the opcode 'add' cannot be performed.
-            This will lead to a value overflow in the EAX register.
-            The maximum value which can be stored is 2,147,483,647""";
-    String actualMessage = exception.getMessage();
-    Assertions.assertEquals(expectedMessage, actualMessage);
-  }
+    @Test
+    void executeMaxIntegerOverflow() {
+        registers.set(EAX, 2147483647);
+        registers.set(EBX, 1);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        Exception exception = assertThrows(ArithmeticException.class, () -> instruction.execute(machine));
+        String expectedMessage = """
+                The combination of values 2147483647 and 1 stored in the registers EAX and EBX using the opcode 'add' cannot be performed.
+                This will lead to a value overflow in the EAX register.
+                The maximum value which can be stored is 2,147,483,647""";
+        String actualMessage = exception.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
 
-  @Test
-  void executeMinIntUnderflow() {
-    registers.set(EAX, -2147483648);
-    registers.set(EBX, -1);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    Exception exception = assertThrows(ArithmeticException.class, () -> instruction.execute(machine));
-    String expectedMessage = """
-            The combination of values -2147483648 and -1 stored in the registers EAX and EBX using the opcode 'add' cannot be performed.
-            This will lead to a value underflow in the EAX register.
-            The minimum value which can be stored is -2,147,483,648""";
-    String actualMessage = exception.getMessage();
-    Assertions.assertEquals(expectedMessage, actualMessage);
-  }
+    @Test
+    void executeMinIntUnderflow() {
+        registers.set(EAX, -2147483648);
+        registers.set(EBX, -1);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        Exception exception = assertThrows(ArithmeticException.class, () -> instruction.execute(machine));
+        String expectedMessage = """
+                The combination of values -2147483648 and -1 stored in the registers EAX and EBX using the opcode 'add' cannot be performed.
+                This will lead to a value underflow in the EAX register.
+                The minimum value which can be stored is -2,147,483,648""";
+        String actualMessage = exception.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
 
 }
